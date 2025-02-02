93db078b843ee8946086a2489d76cb1e432a7462
guilherme.xavier <guilherme.xavier@sap.com>
2025-02-02 01:03:17 -0300

    feat: mini echo modeline

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -335,6 +335,27 @@
   (dashboard-items '())
   (dashboard-banner-logo-title "Welcome to Guimacs!"))
 
+(use-package hide-mode-line
+  :ensure t
+  :hook ((text-mode-hook . hide-mode-line-mode)
+	 (prog-mode-hook . hide-mode-line-mode)))
+
+(use-package dash
+  :ensure t)
+
+(mini-echo-define-segment "persp"
+  "Get the current perspective list"
+  :setup (persp-init-frame)
+  :fetch (mini-echo-segment--extract (frame-parameter nil 'persp--modestring))
+)
+(use-package mini-echo
+  :ensure t
+  :config
+  (mini-echo-mode)
+  :custom
+  (mini-echo-persistent-rule '(:long ("major-mode" "shrink-path" "vcs" "flymake" "persp")
+			       :short ("buffer-name" "flymake"))))
+
 (use-package nerd-icons
   :ensure t
   :defer t)
