e72216c508dfb122460c48f9af8e7ba433d184b0
guilherme.xavier <guilherme.xavier@sap.com>
2025-02-14 21:58:59 -0300

    refactor: early-init file

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -5,34 +5,27 @@
 ;; Emacs early init file.
 
 ;;; Code:
-(defconst emacs-start-time (current-time))
+(setq gc-cons-threshold 10000000)
+(setq byte-compile-warnings '(not obsolete))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
 
-(setq gc-cons-threshold most-positive-fixnum
-      load-prefer-newer noninteractive
-      package-enable-at-startup t
-      package-quickstart t
-      frame-inhibit-implied-resize t
-      frame-resize-pixelwise t
-      load-prefer-newer t
-      native-comp-async-report-warning-errors nil)
+(setq inhibit-startup-echo-area-message (user-login-name))
 
+(setq frame-resize-pixelwise t)
+(tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
+(setq default-frame-alist '((fullscreen . maximized)
 
-(defvar file-name-handler-alist-old file-name-handler-alist)
-(setq file-name-handler-alist nil)
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq file-name-handler-alist file-name-handler-alist-old)))
+                            ;; You can turn off scroll bars by uncommenting these lines:
+                            (vertical-scroll-bars . nil)
+                            (horizontal-scroll-bars . nil)
 
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(push '(mode-line-format . 0) default-frame-alist)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
-
+                            ;; Setting the face in here prevents flashes of
+                            ;; color as the theme gets activated
+                            (background-color . "#000000")
+                            (foreground-color . "#ffffff")
+                            (ns-appearance . dark)
+                            (ns-transparent-titlebar . t)))
 (provide 'early-init)
 
 ;;; early-init.el ends here
